464f5e3704692980e63e51a6cc2da3cfd48ab00f
StillNotWorking <hr.public@lonningdal.net>
2024-03-14 12:25:47 +0100

    Update scaling_governor.sh

--- scaling_governor.sh
+++ scaling_governor.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Adjust CPU speed according to time. Run as cron job.
-# path: /etc/cron.hourly 
+# path: /etc/cron.hourly or simply edit /etc/crontab for these tasks
 # sudo chmod 755 <filename>
 
 hour=$(date +%-H)
 
-if ((9 <= $hour <= 23)); then
+if (($hour >= 9 && $hour <= 22)))); then
     sudo sh -c "echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 else
     sudo sh -c "echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
