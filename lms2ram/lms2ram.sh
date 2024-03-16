74c7c1ae19b843853c1d714716c541914d95a999
StillNotWorking <hr.public@lonningdal.net>
2024-03-16 23:25:49 +0100

    Update lms2ram.sh

--- lms2ram/lms2ram.sh
+++ lms2ram/lms2ram.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
-# Move LMS database files and plugins to RAMdisk
-# Monitor files in directory and copy only new and changed files back to SD card
+# Move LMS database files and plugins to RAMdisk then
+# create symlink where the directory used to live on µSD
+#
+# Monitor files in directory and copy only new and changed 
+# files back to µSD card. Not tested but possible cp -u where only
+# new and file time is tested could provide similar functionality?
+#
 # v0.0.1 - 2024.03.15 - https://github.com/StillNotWorking/LMS-helper-script/tree/main
 
 [[ $@ =~ -v ]] && DEBUG=true
 
-if [ ! $(which inotifywait) ]; then
+if [ ! $(which inotifywait) ]
+then
     [ $DEBUG ] && echo "Install missing inotifywait"
     sudo apt install inotify-tools
 fi
 
-# directory to link and monitor
+# copy this directory to RAMdisk and monitor with inotifywait
 directory="/var/lib/squeezeboxserver/cache"
 
 # files to exlude from inotifywait monitoring
@@ -18,9 +24,9 @@ directory="/var/lib/squeezeboxserver/cache"
 ex="^($directory/spotty/\.tmp.*|$directory/.*\.db-.*)$"
 
 ramsize="2G"
-directorypath=$(dirname $directory)
+directorypath=$(dirname $directory)      # path to symlink
 directorybasename=$(basename $directory) # symlink basename
-passivedirname="$directory~"           # rename original directory
+passivedirname="$directory~"             # rename original directory
 ramdiskpath="/mnt/ramdisk"
 str=""
 
@@ -32,41 +38,51 @@ filecount=$(sudo find $directory -type f,d,l | wc -l)
 [ $DEBUG ] && echo "Filecount: $filecount"
 
 function check_unclean_exit(){
+    [ $DEBUG ] && echo "Check and clean up faulty exit"
     # revert back directory name and rename symlink
-    if [ -L $directory ]; then
+    if [ -L $directory ]
+    then 
         [ $DEBUG ] && echo "Unlink $directory"
         sudo unlink $directory || [ $DEBUG ] && echo "ERROR - Unlink failed: $directory"
     fi
-    if [ -d $passivedirname ]; then
+    if [ -d $passivedirname ]
+    then
         [ $DEBUG ] && echo "Renaming $passivedirname to $directory"
         sudo mv $passivedirname $directory || echo "ERROR - Not able to rename $passivedirname"
     fi
 }
 
 function create_ramdisk() {
+    [ $DEBUG ] && echo "Create mount point and RAMdisk"
     sudo mkdir $ramdiskpath || echo "ERROR - Not able to create directory $ramdiskpath"
     sudo chmod 775 $ramdiskpath
     sudo mount -t tmpfs -o $ramsize lmsramdisk $ramdiskpath 
+    mountpoint -q $ramdiskpath && [ $DEBUG ] && echo "RAMdisk successfully created"
 }
 
-function create_script(){
-    echo ''
-#    echo -e '#!/urc/bin env /bash"' # > ramtofile.sh
-#    echo -e 'IFS='|' read -a fnames <<< "\$str"' # >> ramtofile.sh
-   # for fname in "${fnames[@]}"; do\
-   #     echo "$fname"\
-   #     fullpath="$directory/$fname"\
-   # done") > rmatofile.sh\
+function remove_ramdisk() {
+    [ $DEBUG ] && echo "Umount RAMdisk and remove mount point"
+    if mountpoint -q $ramdiskpath
+    then 
+        sudo umount lmsramdisk || [ $DEBUG ] && echo "ERROR - Could not umount 'lmsramdisk'"
+        sudo rm -r $ramdiskpath || [ $DEBUG ] && echo "ERROR - Not able to remove directory $ramdiskpath"
+    else
+        [ $DEBUG ] && echo "ERROR - $ramdiskpath is not a mount point"
+    fi
 }
-#create_script;ls -l;exit;
+
 function copy_files(){
-    # copy changed files from ramdisk to storage
-    #if [ -d $passivedirname ]; then
-    IFS='|' read -a fnames <<< "$str"
-    for fname in "${fnames[@]}"; do
-        echo "$fname"
-        fullpath="$directory/$fname"
-    done
+    [ $DEBUG ] && echo "Copy new and changed files from RAMdisk to storage"
+    if [ -d $passivedirname ]
+    then
+        IFS='|' read -a fnames <<< "$str"
+        for fname in "${fnames[@]}"
+        do
+            fullpath="$directory/$fname"
+            sudo cp -rp $fullpath $passivedirname
+            [ $DEBUG ] && echo "Copy $fullpath"
+        done
+    fi
 }
 
 function do_before_exit() {
@@ -74,37 +90,35 @@ function do_before_exit() {
     newfilecount=$(sudo find $directory -type f,d,l | wc -l)
     [ $DEBUG ] && echo "Start filecount: $filecount  -  End filecount: $newfilecount"
 
-    [ $DEBUG ] && echo "Stop LMS to close all temporary SQLite files"
+    [ $DEBUG ] && echo "Stop LMS will close all temporary SQLite files"
     sudo systemctl stop logitechmediaserver
 
-    [ $DEBUG ] && echo "Copy files from RAMdisk to µSD card"
     copy_files
 
-    # check if directory is symlink, and if the renamed directory exist
-    # if not we are careful not deleting anything
-    if [ -L $directory ] && [ -d $passivedirname ] ; then
+    # careful not delete anything we shouldn't we check if directory 
+    # actually is a symlink, and if the renamed directory exist
+    if [ -L $directory ] && [ -d $passivedirname ]
+    then
         [ $DEBUG ] && echo "Remove symbolic link"
         sudo unlink $directory
-        [ $DEBUG ] && echo "Change name back back to original: $directory"
+        [ $DEBUG ] && echo "Change directory name back back to original: $directory"
         sudo mv $passivedirname $directory
     fi
 
-    [ $DEBUG ] && echo "Unount RAMdisk"
-    sudo umount lmsramdisk
+    remove_ramdisk
 
     [ $DEBUG ] && echo "Start Logitech Media Server from standard drive"
     sudo systemctl start logitechmediaserver
 
-    [ $DEBUG ] && echo "CPU speed back to default 'ondemand'"
+    [ $DEBUG ] && echo "Set CPU speed back to default 'ondemand'"
     sudo sh -c "echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 
-    [ $DEBUG ] && echo "Exit PID: $BASHPID"
+    [ $DEBUG ] && echo "Successful exit $0 PID: $BASHPID"
     exit 0
 }
 
 function initiate_program() {
 
-    [ $DEBUG ] && echo "Check and clean up faulty exit"
     check_unclean_exit
 
     [ $DEBUG ] && echo "Set CPU scaling governor (speed) to 'performance'"
@@ -113,7 +127,6 @@ function initiate_program() {
     [ $DEBUG ] && echo "Stop Logitech Media Server"
     sudo systemctl stop logitechmediaserver
 
-    [ $DEBUG ] && echo "Create RAMdisk"
     create_ramdisk
 
     [ $DEBUG ] && echo "Change name on original $directory"
